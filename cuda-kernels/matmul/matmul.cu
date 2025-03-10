625fa47343b7dbbbb21ee61bad2a08bdd91ca8a2
Oliver <olivergestanley@gmail.com>
2025-03-10 19:20:31 +0000

    matmul with increased use of registers

--- cuda-kernels/matmul/matmul.cu
+++ cuda-kernels/matmul/matmul.cu
@@ -1,62 +1,91 @@
-// TODO: further optimise using more registers & vectorized memory accesses
+// TODO: further optimise using vectorized memory accesses
 
 #include <assert.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <torch/extension.h>
 
-#define ELEMENTS_PER_THREAD 8
-// Tiles of matrix A
-#define tiles_A_rows 64
-#define tiles_A_cols 8
-// Tiles of matrix B
-#define tiles_B_cols 64
+// first implementation just used 8 elements per thread
+// to utilise registers more heavily, we use 8x8
+#define ROWS_PER_THREAD 8
+#define COLS_PER_THREAD 8
+
+// rows & cols per tile of matrix A
+#define tiles_A_rows 128
+#define tiles_A_cols 16
+
+// cols per tile of matrix B
+#define tiles_B_cols 128
 
 __global__ void matmul_kernel(float *d_A_ptr, float *d_B_ptr, float *d_C_ptr, int C_n_rows, int C_n_cols, int A_n_cols) {
+    const int n_threads_per_block = tiles_A_rows * tiles_B_cols / (ROWS_PER_THREAD * COLS_PER_THREAD);
+    static_assert(n_threads_per_block % tiles_A_cols == 0);
+    static_assert(n_threads_per_block % tiles_B_cols == 0);
+
+    // per-thread view indices for loading A tiles
     const int A_view_ty = threadIdx.x / tiles_A_cols;
     const int A_view_tx = threadIdx.x % tiles_A_cols;
+    const int A_stride = n_threads_per_block / tiles_A_cols;
+
+    // per-thread view indices for loading B tiles
     const int B_view_ty = threadIdx.x / tiles_B_cols;
     const int B_view_tx = threadIdx.x % tiles_B_cols;
+    const int B_stride = n_threads_per_block / tiles_B_cols;
 
-    const int row_start = tiles_A_rows * blockIdx.y + ELEMENTS_PER_THREAD * (threadIdx.x / tiles_B_cols);
-    const int col = tiles_B_cols * blockIdx.x + (threadIdx.x % tiles_B_cols);
+    // starting indices for the registers that accumulate the dot product
+    const int row_start = COLS_PER_THREAD * (threadIdx.x / (tiles_B_cols / ROWS_PER_THREAD));
+    const int col_start = ROWS_PER_THREAD * (threadIdx.x % (tiles_B_cols / ROWS_PER_THREAD));
 
-    // repeated memory accesses to global memory are slow -> use shared memory
+    // repeated memory accesses to global memory are slow -> use shared memory for tiles
     // shared memory is small -> use tiles rather than the whole matrix
     __shared__ float A_shared[tiles_A_rows][tiles_A_cols];
     __shared__ float B_shared[tiles_A_cols][tiles_B_cols];
 
     const int num_tiles = ceil(A_n_cols / (float) tiles_A_cols);
 
-    // accumulate ELEMENTS_PER_THREAD values in a register before writing to global memory
-    float value[ELEMENTS_PER_THREAD] = {0.0f};
+    // accumulate COLS_PER_THREADxROWS_PER_THREAD values in registers before writing to global memory
+    float value[COLS_PER_THREAD][ROWS_PER_THREAD] = {0.0f};
+
+    float A_temp[ROWS_PER_THREAD] = {0.0f};
+    float B_temp[COLS_PER_THREAD] = {0.0f};
 
     for (int tile = 0; tile < num_tiles; tile++) {
-        // load tiles into shared memory
-        int aRow = blockIdx.y * tiles_A_rows + A_view_ty;
-        int aCol = tile * tiles_A_cols + A_view_tx;
-        if (aRow < C_n_rows && aCol < A_n_cols) {
-            A_shared[A_view_ty][A_view_tx] = d_A_ptr[aRow * A_n_cols + aCol];
-        } else {
-            A_shared[A_view_ty][A_view_tx] = 0.0f;
+        // load tiles into shared memory first
+        for (int offset = 0; offset < tiles_A_rows; offset += A_stride) {
+            int global_row = blockIdx.y * tiles_A_rows + offset + A_view_ty;
+            int global_col = tile * tiles_A_cols + A_view_tx;
+            if (global_row < C_n_rows && global_col < A_n_cols) {
+                A_shared[offset + A_view_ty][A_view_tx] = d_A_ptr[global_row * A_n_cols + global_col];
+            } else {
+                A_shared[offset + A_view_ty][A_view_tx] = 0.0f;
+            }
         }
-        int bRow = tile * tiles_A_cols + B_view_ty;
-        int bCol = blockIdx.x * tiles_B_cols + B_view_tx;
-        if (bRow < A_n_cols && bCol < C_n_cols) {
-            B_shared[B_view_ty][B_view_tx] = d_B_ptr[bRow * C_n_cols + bCol];
-        } else {
-            B_shared[B_view_ty][B_view_tx] = 0.0f;
+        for (int offset = 0; offset < tiles_A_cols; offset += B_stride) {
+            int global_row = tile * tiles_A_cols + B_view_ty + offset;
+            int global_col = blockIdx.x * tiles_B_cols + B_view_tx;
+            if (global_row < A_n_cols && global_col < C_n_cols) {
+                B_shared[B_view_ty + offset][B_view_tx] = d_B_ptr[global_row * C_n_cols + global_col];
+            } else {
+                B_shared[B_view_ty + offset][B_view_tx] = 0.0f;
+            }
         }
         // sync ensures all threads have loaded tiles into shared memory
         __syncthreads();
 
         for (int j = 0; j < tiles_A_cols; j++) {
-            // load to a register before loop, avoiding repeated shared memory accesses
-            float B_val = B_shared[j][B_view_tx];
+            // load the block into registers, avoiding repeated shared memory accesses
+            for (int i = 0; i < ROWS_PER_THREAD; i++) {
+                A_temp[i] = A_shared[row_start + i][j];
+            }
+            for (int i = 0; i < COLS_PER_THREAD; i++) {
+                B_temp[i] = B_shared[j][col_start + i];
+            }
 
             // compute dot product
-            for (int c = 0; c < ELEMENTS_PER_THREAD; c++) {
-                value[c] += A_shared[B_view_ty * ELEMENTS_PER_THREAD + c][j] * B_val;
+            for (int y = 0; y < COLS_PER_THREAD; y++) {
+                for (int x = 0; x < ROWS_PER_THREAD; x++) {
+                    value[y][x] += A_temp[y] * B_temp[x];
+                }
             }
         }
         // sync ensures all threads have finished computation
@@ -64,9 +93,13 @@ __global__ void matmul_kernel(float *d_A_ptr, float *d_B_ptr, float *d_C_ptr, in
     }
 
     // finally, write to global memory
-    for (int c = 0; c < ELEMENTS_PER_THREAD; c++) {
-        if (row_start + c < C_n_rows && col < C_n_cols) {
-            d_C_ptr[(row_start + c) * C_n_cols + col] = 1 * value[c] + 0 * d_C_ptr[(row_start + c) * C_n_cols + col];
+    for (int y = 0; y < COLS_PER_THREAD; y++) {
+        for (int x = 0; x < ROWS_PER_THREAD; x++) {
+            int global_row = blockIdx.y * tiles_A_rows + row_start + y;
+            int global_col = blockIdx.x * tiles_B_cols + col_start + x;
+            if (global_row < C_n_rows && global_col < C_n_cols) {
+                d_C_ptr[global_row * C_n_cols + global_col] = 1 * value[y][x] + 0 * d_C_ptr[global_row * C_n_cols + global_col];
+            }
         }
     }
 }
@@ -76,7 +109,7 @@ void matmul(torch::Tensor a, torch::Tensor b, torch::Tensor out) {
     int C_n_cols = b.size(1);
     int A_n_cols = a.size(1);
 
-    dim3 dim_block(tiles_A_rows * tiles_B_cols / ELEMENTS_PER_THREAD);
+    dim3 dim_block(tiles_A_rows * tiles_B_cols / (ROWS_PER_THREAD * COLS_PER_THREAD));
     dim3 dim_grid(ceil(C_n_cols / (float)(tiles_B_cols)), ceil(C_n_rows / (float)(tiles_A_rows)));
     matmul_kernel<<<dim_grid, dim_block>>>(a.data_ptr<float>(), b.data_ptr<float>(), out.data_ptr<float>(), C_n_rows, C_n_cols, A_n_cols);
 }
